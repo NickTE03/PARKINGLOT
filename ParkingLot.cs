4971c13d196927c89edec7db24a1b2a351819bbb
NickTE03 <68023365+NickTE03@users.noreply.github.com>
2021-04-08 23:55:00 -0400

    Add files via upload

---
+++ ParkingLot.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PARKINGLOT
+{
+    class PARKINGLOT
+    {
+        private int Large;
+        private int Medium;
+        private int Small;
+
+
+        public PARKINGLOT(int Large, int Medium, int Small)
+        {
+            this.Large = Large;
+            this.Medium = Medium;
+            this.Small = Small;
+        }
+
+        public bool AddCar(string carType)
+        {
+            bool check = false;
+            carType = carType.ToLower();
+
+            if (carType == "large" && Large > 0)
+            {
+                check = true;
+                Large--;
+            }
+            else if (carType == "medium" && Medium > 0)
+            {
+                check = true;
+                Medium--;
+            }
+            else if (carType == "small" && Small > 0)
+            {
+                check = true;
+                Small--;
+            }
+            return check;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PARKINGLOT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PARKINGLOT PARKINGSPOT1 = new PARKINGLOT(1, 1, 1);
+            Console.WriteLine(PARKINGSPOT1.AddCar("Large"));
+            Console.WriteLine(PARKINGSPOT1.AddCar("Large"));
+            Console.WriteLine(PARKINGSPOT1.AddCar("Medium"));
+            Console.WriteLine(PARKINGSPOT1.AddCar("Small"));
+            Console.WriteLine(PARKINGSPOT1.AddCar("Small"));
+        }
+    }
+}
